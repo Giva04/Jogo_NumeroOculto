aa1d1352da6689bb4db9748e733471fbad14a03e
Giva04 <givagomes04@gmail.com>
2023-05-02 21:36:16 -0300

    Criando Logiga para armazenar os numero do usuario e mostrar o Palpite

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+// VARIAVEL DO NUMERO DO COMPUTADOR
 let computerNumber
+
+//VARIAVEL QUE ARMEZA NA ARRAY O NUMERO DIGITADO 
+let userNumbers = []
 
 // FUNÇÃO QUE INICIALIZA  E REINICIALIZA O JOGO
 
 function init( ) {
-    computerNumber = Math.floor(Math.random() *100 +1)
-    console.log( computerNumber)
+    // variavel que armazena o numero do computador
+    computerNumber = Math.floor(Math.random() * 100 + 1)
+     console.log( computerNumber)
+}
+// FUNÇÃO QUE AMAZENA O NUMERO DIGITADO NO INPUT na array
+function compareNumbers() {
+   const userNumber = Number(document.getElementById('inputBox').value)
+//    para enviar o numero para array
+   userNumbers.push(' ' + userNumber)
+
+// para fazer paracer o numero digitado no palpite
+    document.getElementById('guesses').innerHTML = userNumbers
 }

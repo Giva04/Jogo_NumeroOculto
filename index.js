99e051bb9d80af297a55c744479d22f86722e0af
Giva04 <givagomes04@gmail.com>
2023-05-02 21:06:22 -0300

    Desenvolvendo Html e Css do Jogo

---
+++ index.js
@@ -0,0 +1,8 @@
+let computerNumber
+
+// FUNÇÃO QUE INICIALIZA  E REINICIALIZA O JOGO
+
+function init( ) {
+    computerNumber = Math.floor(Math.random() *100 +1)
+    console.log( computerNumber)
+}

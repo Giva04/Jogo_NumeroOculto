e46c4676ccd6f22b8c1a8184e0229ac7ae41009e
Giva Gomes <givagomes04@gmail.com>
2023-05-08 23:01:19 -0300

    Introduzindo  alerta de numero invalido

--- index.js
+++ index.js
@@ -10,15 +10,12 @@ let attempts = 0
 // VARIAVEL QUE LIMITA O NUMERO DE TENTATIVAS
 let maxguesses = 10 
 
-
 // FUNÇÃO PARA VALIDAR O BOTÃO NOVO JOGO
 function newGame() {
     window.location.reload()
 }
 
-
 // FUNÇÃO QUE INICIALIZA  E REINICIALIZA O JOGO
-
 function init( ) {
     // variavel que armazena o numero do computador
     computerNumber = Math.floor(Math.random() * 100 + 1)
@@ -29,6 +26,11 @@ function compareNumbers() {
    const userNumber = Number(document.getElementById('inputBox').value)
 //    para enviar o numero para array
    userNumbers.push(' ' + userNumber)
+// alerta de numero invalido  
+   if(userNumber > 100 || userNumber <= 0 ){
+        alert('Numero invalido');
+        return
+   } 
 
 // para fazer paracer o numero digitado no palpite
     document.getElementById('guesses').innerHTML = userNumbers
@@ -36,35 +38,35 @@ function compareNumbers() {
 // LOGICA PARA LIMITAR O NUMERO DE TENTATIVAS
     if (attempts < maxguesses) {
        // CRIANDO A LOGICA PARA O COMPUTADOR ESCOLHER NUMERO, E DAR DICAS DE APROXIMAÇAO DO NUMERO ESCOLHIDO PELO COMUTADOR
-    if(userNumber > computerNumber) {
-        // logica que da dida de aproximação do numero
-     document.getElementById('textOutput').innerHTML = 'Seu Numero é maior'
+        if(userNumber > computerNumber) {
+            // logica que da dida de aproximação do numero
+        document.getElementById('textOutput').innerHTML = 'Seu Numero é maior'
+        
+        // logica para apagar no numero  digitado quando for o errado
+        document.getElementById('inputBox').value = ''
+                
+        // logica par contabilizar as tentativas
+        attempts ++
+        document.getElementById('attempts').innerHTML = attempts
 
-       // logica para apagar no numero  digitado quando for o errado
-     document.getElementById('inputBox').value = ''
+        }
+        else if(userNumber < computerNumber) {        
+        document.getElementById('textOutput').innerHTML = 'Seu Numero é menor'
 
-     // logica par contabilizar as tentativas
-     attempts ++
-     document.getElementById('attempts').innerHTML = attempts
-
-    }
-    else if(userNumber < computerNumber) {        
-     document.getElementById('textOutput').innerHTML = 'Seu Numero é menor'
-
-     document.getElementById('inputBox').value = ''
-
-     attempts ++
-     document.getElementById('attempts').innerHTML = attempts
-    }
-    else {
-        document.getElementById('textOutput').innerHTML = 'Você acertou!!!!!!'
+        document.getElementById('inputBox').value = ''
 
         attempts ++
         document.getElementById('attempts').innerHTML = attempts
-        // logica para não receber mais numeros acertar o numero Oculto
-        document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
-    }
+        }
+        else {
+            document.getElementById('textOutput').innerHTML = 'Você acertou!!!!!!'
 
+            attempts ++
+            document.getElementById('attempts').innerHTML = attempts
+            // logica para não receber mais numeros acertar o numero Oculto
+            document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
+        }
+     
   }
         // LOGICA PARA INFORMAR QUE VOCÊ PERDEU E REVELAR O NUMERO OCULTO
     else {

dd29c26c37ce73d9b5f97f9663845be7e9160076
Giva Gomes <givagomes04@gmail.com>
2023-05-08 21:49:04 -0300

    Introduzindo Logica para limitar as tentativas e revelar o numero Oculto

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ let computerNumber
 //VARIAVEL QUE ARMEZA NA ARRAY O NUMERO DIGITADO 
 let userNumbers = []
 
-// VARIAVEL QUE CONTABILIZA O NUMERO DE TENTATICAS
+// VARIAVEL QUE CONTABILIZA O NUMERO DE TENTATIVAS
 let attempts = 0
+
+// VARIAVEL QUE LIMITA O NUMERO DE TENTATIVAS
+let maxguesses = 10 
+
 
 // FUNÇÃO QUE INICIALIZA  E REINICIALIZA O JOGO
 
@@ -23,11 +27,13 @@ function compareNumbers() {
 // para fazer paracer o numero digitado no palpite
     document.getElementById('guesses').innerHTML = userNumbers
 
-// CRIANDO A LOGICA PARA O COMPUTADOR ESCOLHER NUMERO, E DAR DICAS DE APROXIMAÇAO DO NUMERO ESCOLHIDO PELO COMUTADOR
+// LOGICA PARA LIMITAR O NUMERO DE TENTATIVAS
+    if (attempts < maxguesses) {
+       // CRIANDO A LOGICA PARA O COMPUTADOR ESCOLHER NUMERO, E DAR DICAS DE APROXIMAÇAO DO NUMERO ESCOLHIDO PELO COMUTADOR
     if(userNumber > computerNumber) {
         // logica que da dida de aproximação do numero
      document.getElementById('textOutput').innerHTML = 'Seu Numero é maior'
-     
+
        // logica para apagar no numero  digitado quando for o errado
      document.getElementById('inputBox').value = ''
 
@@ -51,4 +57,9 @@ function compareNumbers() {
         document.getElementById('attempts').innerHTML = attempts
     }
 
+  }
+        // LOGICA PARA INFORMAR QUE VOCÊ PERDEU E REVELAR O NUMERO OCULTO
+    else {
+        document.getElementById('textOutput').innerHTML ='Ahhh!!! Você Perdeu. O número Oculto era:' + computerNumber
+    }
 }
